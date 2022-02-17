2b935e7c5e649230d2e983c3ca2cf415b9d46573
Ali Nouri <dynamic11@gmail.com>
2022-02-17 10:14:26 -0500

    add form validation

--- src/pages/ExampleFormPage/ExampleFormPage.tsx
+++ src/pages/ExampleFormPage/ExampleFormPage.tsx
@@ -1,38 +1,108 @@
 import { FormattedMessage } from 'react-intl';
-import { Title, Button, Form } from '@dynamic11/wet-react';
+import { Title, Button, Form, Label } from '@dynamic11/wet-react';
+import { useForm, Controller } from 'react-hook-form';
+import { ErrorMessage } from '@hookform/error-message';
 import '../../App.css';
 
-const handleFormSubmit = (event: any) => {
-  event.preventDefault();
-  console.log('submitted', event.target.elements.formBasicEmail.value);
+// const handleFormSubmit = (event: any) => {
+//   event.preventDefault();
+//   console.log('submitted', event.target.elements.formBasicEmail.value);
+// };
+
+type FormInputType = {
+  formEmail: string;
+  formPass: string;
+  formCheckbox: boolean;
 };
 
-const ExampleFormPage = () => (
-  <div>
-    <Title level="h1">
-      <FormattedMessage id="menu.example.form" />
-    </Title>
-    <Form onSubmit={handleFormSubmit}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We will never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+const ExampleFormPage = () => {
+  const {
+    handleSubmit,
+    watch,
+    control,
+    formState: { errors },
+  } = useForm<FormInputType>();
+  const onSubmit = handleSubmit((data) => console.log(data));
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
-  </div>
-);
+  console.log('email:', watch('formEmail')); // watch input value by passing the name of it
+  console.log('pass:', watch('formPass')); // watch input value by passing the name of it
+  console.log('check:', watch('formCheckbox'));
+
+  return (
+    <>
+      <Title level="h1">
+        <FormattedMessage id="menu.example.form" />
+      </Title>
+      <Form onSubmit={onSubmit}>
+        <Form.Group className="mb-3" controlId="formEmail">
+          <Form.Label>Email address</Form.Label>
+          <Controller
+            name="formEmail"
+            control={control}
+            rules={{ required: { value: true, message: 'This is required.' } }}
+            defaultValue="default@email.com"
+            render={({ field }) => (
+              <Form.Control {...field} type="email" placeholder="Enter email" />
+            )}
+          />
+          <Form.Text className="text-muted">
+            We will never share your email with anyone else.
+          </Form.Text>
+          <ErrorMessage
+            errors={errors}
+            name="formEmail"
+            as={<Label variant="danger" />}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formPass">
+          <Form.Label>Password</Form.Label>
+          <Controller
+            name="formPass"
+            control={control}
+            rules={{
+              required: { value: true, message: 'This is required.' },
+              maxLength: {
+                value: 10,
+                message: 'Must be less than 10 characters',
+              },
+              minLength: {
+                value: 4,
+                message: 'Must be more than 4 characters',
+              },
+            }}
+            render={({ field }) => (
+              <Form.Control {...field} type="password" placeholder="Password" />
+            )}
+          />
+          <ErrorMessage
+            errors={errors}
+            name="formPass"
+            as={<Label variant="danger" />}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formCheckbox">
+          <Controller
+            name="formCheckbox"
+            control={control}
+            rules={{ required: { value: true, message: 'This is required.' } }}
+            render={({ field }) => (
+              <Form.Check {...field} type="checkbox" label="Check me out" />
+            )}
+          />
+          <ErrorMessage
+            errors={errors}
+            name="formCheckbox"
+            as={<Label variant="danger" />}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
+};
 
 export default ExampleFormPage;
